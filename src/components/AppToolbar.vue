14e5fbb5755638b076c8c9ead79dc2ae02cb5283
pedja77 <pedja77@stcable.net>
2018-05-29 21:11:03 +0200

    Z2 - AppToolbar

---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-ons-toolbar>
+    <div class="left">
+      <slot name="left"></slot>
+    </div>
+    <div class="center">
+      {{ title }}
+    </div>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </v-ons-toolbar>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "Github App"
+    }
+  }
+}
+</script>
+
+<style>
+</style>
