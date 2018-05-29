2e1b341a04258dde6cb1e1f259956fcd4c1bcca4
pedja77 <pedja77@stcable.net>
2018-05-29 21:35:59 +0200

    Z3 - AppSearch

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <v-ons-page>
     <app-toolbar />
+    <app-search :query.sync="query" placeholder="Search GitHub" /> {{ query }}
   </v-ons-page>
 
 </template>
 <script>
 import AppToolbar from "./components/AppToolbar.vue"
+import AppSearch from "./components/AppSearch.vue"
 
 export default {
   components: {
-    AppToolbar
+    AppToolbar,
+    AppSearch
   },
   data() {
     return {
-      title: "Github app"
+      query: ""
     }
   },
   methods: {}
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-ons-search-input :placeholder="placeholder" style="width: 100%;" :value="query" @input="$emit('update:query', $event.target.value)">
+  </v-ons-search-input>
+</template>
+
+<script>
+export default {
+  props: {
+    query: {
+      type: String
+    },
+    placeholder: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style>
+</style>
