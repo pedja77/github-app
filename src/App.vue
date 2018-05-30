905600d568ff154dde049e221c37aca13ea382b3
pedja77 <pedja77@stcable.net>
2018-05-30 14:07:42 +0200

    Z5 - query watcher

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <v-ons-page>
     <app-toolbar />
-    <app-search :query.sync="query" placeholder="Search GitHub" />
-    <v-ons-button @click="getRepos">Search</v-ons-button>
+    <app-search :query.sync="query" placeholder="Search GitHub" @update:query="updateList" />
   </v-ons-page>
 
 </template>
@@ -10,6 +9,7 @@
 import AppToolbar from "./components/AppToolbar.vue"
 import AppSearch from "./components/AppSearch.vue"
 import { gitHub } from "./services/GitHub.js"
+import _ from "lodash"
 
 export default {
   components: {
@@ -18,14 +18,27 @@ export default {
   },
   data() {
     return {
-      query: ""
+      query: "",
+      repos: []
     }
+  },
+  watch: {
+    query: function() {
+      this.debouncedGetRepos()
+    }
+  },
+  created() {
+    this.debouncedGetRepos = _.debounce(this.getRepos, 500)
   },
   methods: {
     getRepos() {
-      gitHub.getRepos(this.query).then(response => {
-        console.log(response)
+      gitHub.getRepos(this.query).then(({ data }) => {
+        this.repos = data
+        console.log(this.repos)
       })
+    },
+    updateList() {
+      _.debounce(this.getRepos, 500)
     }
   }
 }
