2030a0db8c9a51ffee2bdb4aa729ea61bc2ac430
pedja77 <pedja77@stcable.net>
2018-05-31 19:12:12 +0200

    Final touches, for now, again

--- src/services/GitHub.js
+++ src/services/GitHub.js
@@ -2,10 +2,11 @@ import axios from "axios"
 
 const API_TOKEN = "1461f25375db0eb42e62493cca59f65152326554"
 
-axios.defaults.baseURL = "https://api.github.com/users/"
-axios.defaults.headers.common["Authorization"] = `Bearer ${API_TOKEN}`
-
 class GitHub {
+  constructor() {
+    axios.defaults.baseURL = "https://api.github.com/users/"
+    axios.defaults.headers.common["Authorization"] = `Bearer ${API_TOKEN}`
+  }
   getRepos(username) {
     return axios.get(`/${username}/repos`)
   }
