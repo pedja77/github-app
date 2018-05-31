4c2bcaf9e99024a5d26365aaa14f92500a821908
pedja77 <pedja77@stcable.net>
2018-05-31 19:07:48 +0200

    Final touches, for now

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
 <template>
   <v-ons-page>
-    <app-toolbar />
-    <app-search :query.sync="query" placeholder="Search GitHub" @update:query="debouncedGetRepos" />
-    <div>
-      <div v-if="requestStatus == 404">
-        <user-not-found />
-      </div>
-      <div v-else>
-        <v-ons-list v-if="showList">
-          <v-ons-list-item v-for="(repo, index) in repos" :key="index">
-            <v-ons-row>
-              <v-ons-col width="30%">
-                <img :src="makeAvatarUrl(query)" width="50px" />
-              </v-ons-col>
-              <v-ons-col>
-                <h4>{{ repo.name }}</h4>
-                <p>{{ repo.description }}</p>
-              </v-ons-col>
-            </v-ons-row>
-          </v-ons-list-item>
-        </v-ons-list>
-        <empty-state v-else type="repo" />
-      </div>
+    <app-toolbar>
+    </app-toolbar>
+    <div class="content">
+      <app-search :query.sync="query" placeholder="Search GitHub" @update:query="debouncedGetRepos" />
+      <div>
+        <div v-if="requestStatus == 404">
+          <user-not-found />
+        </div>
+        <div v-else>
+          <v-ons-list v-if="showList">
+            <v-ons-list-header>Repositories of {{ query }}</v-ons-list-header>
+            <v-ons-list-item v-for="(repo, index) in repos" :key="index">
+              <v-ons-row>
+                <v-ons-col width="30%">
+                  <img :src="repo.owner.avatar_url" width="50px" />
+                </v-ons-col>
+                <v-ons-col>
+                  <h4>{{ repo.name }}</h4>
+                  <p>{{ repo.description }}</p>
+                </v-ons-col>
+              </v-ons-row>
+            </v-ons-list-item>
+          </v-ons-list>
+          <empty-state v-else type="repo" />
+        </div>
 
-    </div>
-    <div v-if="fetching" class="progress">
-      <v-ons-progress-circular indeterminate></v-ons-progress-circular>
+      </div>
+      <div v-if="fetching" id="progress">
+        <v-ons-progress-circular indeterminate></v-ons-progress-circular>
+      </div>
     </div>
   </v-ons-page>
 
@@ -37,7 +41,7 @@ import EmptyState from "./components/EmptyState.vue"
 import UserNotFound from "./components/UserNotFound.vue"
 
 import { gitHub } from "./services/GitHub.js"
-import _ from "lodash"
+import debounce from "lodash/debounce"
 
 export default {
   components: {
@@ -51,13 +55,15 @@ export default {
       query: "",
       repos: [],
       fetching: false,
-      //isUser: true,
       requestStatus: null
     }
   },
   watch: {
     query: function() {
       this.debouncedGetRepos()
+    },
+    fetching: function(val) {
+      console.log(val)
     }
   },
   computed: {
@@ -66,38 +72,42 @@ export default {
     }
   },
   created() {
-    this.debouncedGetRepos = _.debounce(this.getRepos, 500)
+    this.debouncedGetRepos = debounce(this.getRepos, 500)
   },
   methods: {
     getRepos() {
       this.fetching = true
-      //console.log("getReposIn", this.fetching)
       gitHub
         .getRepos(this.query)
         .then(({ data }) => {
           this.requestStatus = null
           this.repos = data
-          this.fetching = false
-          console.log(this.repos)
         })
         .catch(err => {
           this.requestStatus = err.response.status
-          console.log("err", err.response)
         })
-
-      //console.log("getReposOut", this.fetching)
-    },
-    makeAvatarUrl(query) {
-      return this.repos.length ? `https://github.com/${query}.png` : "#"
+        .finally(() => {
+          this.fetching = false
+        })
     }
   }
 }
 </script>
 
 <style>
-.progress {
-  vertical-align: middle;
-  margin: 0 auto;
+#progress {
+  position: fixed;
+  width: 20%; /* Set your desired with */
+  z-index: 2; /* Make sure its above other items. */
+  top: 50%;
+  left: 50%;
+  margin-top: -10%; /* Changes with height. */
+  margin-left: -10%; /* Your width divided by 2. */
+  /* You will not need the below, its only
+           for styling   purposes.*/
+  padding: 10px;
+  border: none;
+  text-align: center;
 }
 </style>
 
--- src/services/GitHub.js
+++ src/services/GitHub.js
@@ -1,6 +1,9 @@
 import axios from "axios"
 
+const API_TOKEN = "1461f25375db0eb42e62493cca59f65152326554"
+
 axios.defaults.baseURL = "https://api.github.com/users/"
+axios.defaults.headers.common["Authorization"] = `Bearer ${API_TOKEN}`
 
 class GitHub {
   getRepos(username) {
